cad8c83d5293e63873f9f8be20a76a3dcf538a91
Jörg Thalheim <joerg@thalheim.io>
2024-06-08 14:01:16 +0200

    add missing templates to flake.nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,16 @@
         path = ./single-disk-ext4;
         description = "Single disk ext4";
       };
+
+      single-ext4-luks-and-double-zfs-mirror = {
+        path = ./single-ext4-luks-and-double-zfs-mirror;
+        description = "Root on ext4 with LUKS encryption and a zfs mirror raid";
+      };
+
+      zfs-impermanence = {
+        path = ./zfs-impermanence;
+        description = "ZFS Impermanence. See https://grahamc.com/blog/erase-your-darlings/";
+      };
     };
   };
 }
